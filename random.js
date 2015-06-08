c84601290d4d1ddfb4db7d760d88d0d0036ee1d7
raquelrodriguezb <raquel.rodriguezb@gmail.com>
2015-06-08 12:14:09 +0200

    random versión 2

--- random.js
+++ random.js
@@ -1,7 +1,8 @@
 var numero=Math.random();
+var str=' MAYOR QUE 0,5 ';
 if (numero<=0.5){
 console.log('\n'+ numero+  ' MENOR QUE 0,5 \n');
 
 }else{
-console.log('\n'+ numero+  ' MAYOR QUE 0,5 \n');
+console.log('\n'+ numero+  str+' \n');
 }
